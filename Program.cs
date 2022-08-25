10712b58e25de3211e6e89553b0f57fc8c7e6e1b
mhmtciftci <mhmtciftci@protonmail.com>
2022-08-25 21:16:31 +0300

    updated

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Ödev 1 
+        Console.WriteLine("Bir Sayı Giriniz: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[] sayilar= new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine(i+1 +". Pozitif Sayıyı giriniz");
+            sayilar[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.WriteLine("Çift Olan Sayılar");
+        foreach (var item in sayilar)
+        {
+            if(item%2==0)
+                Console.WriteLine(item);            
+        }
+    }
+}
