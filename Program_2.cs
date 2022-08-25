d94abb5dffe46a0a15e0e8bf99af4e29c925d850
mhmtciftci <mhmtciftci@protonmail.com>
2022-08-25 21:25:41 +0300

    updated

--- Program_2.cs
+++ Program_2.cs
@@ -1,22 +1,25 @@
-﻿internal class Program
+﻿internal class Yenipogram
 {
     private static void Main(string[] args)
     {
-        // Ödev 1 
-        Console.WriteLine("Bir Sayı Giriniz: ");
+        // Ödev 2 
+        Console.WriteLine("Pozitif iki Sayı Giriniz. İlk Rakam: ");
         int n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Pozitif iki Sayı Giriniz. İkinci Rakam: ");
+        int m = Convert.ToInt32(Console.ReadLine());
+        
         int[] sayilar= new int[n];
-
+        Console.WriteLine(n + " Pozitif sayı giriniz.");
         for (int i = 0; i < n; i++)
         {
             Console.WriteLine(i+1 +". Pozitif Sayıyı giriniz");
             sayilar[i] = Convert.ToInt32(Console.ReadLine());
         }
 
-        Console.WriteLine("Çift Olan Sayılar");
+        Console.WriteLine(m + "ile bölünen ve eşit sayılar");
         foreach (var item in sayilar)
         {
-            if(item%2==0)
+            if(item%m==0)
                 Console.WriteLine(item);            
         }
     }
