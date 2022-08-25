74014d889bf5324d9c07ce0439e0efe9923df069
mhmtciftci <mhmtciftci@protonmail.com>
2022-08-25 22:17:22 +0300

    updated

--- Program_4.cs
+++ Program_4.cs
@@ -5,7 +5,14 @@
         // Ödev 4 
         Console.WriteLine("Bir Cümle Giriniz: ");
         string cumle = Console.ReadLine();
-
         
+        string cumle2 = cumle.Replace(" ","");
+        int uz = cumle2.Length;
+        Console.WriteLine("Toplam harf Sayısı: "+ uz);
+
+        string[] cumle3 = cumle.Split(' ');
+
+        int kelime = cumle3.Count();
+        Console.WriteLine("Toplam kelime Sayısı: "+ kelime);
     }
 }
